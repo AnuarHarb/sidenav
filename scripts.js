527f81a957e86391e72f1815e20f1246f01c66bc
Marco Vinicio <mvglizarraga@gmail.com>
2019-06-12 08:56:25 -0500

    document listener only when sidenav is open

--- scripts.js
+++ scripts.js
@@ -1,23 +1,27 @@
 'use strict';
 
-
-
-
 const sidenavTriggers = document.querySelectorAll('.sidenav-trigger');
 const sidenav = document.getElementById('sidenav');
 
-document.addEventListener('click', hideSidenav);
+// document.addEventListener('click', hideSidenav);
 
 for(let trigger of sidenavTriggers) {
   trigger.addEventListener('click', toggleSidenav);
 }
 
 function toggleSidenav() {
-  sidenav.classList.toggle('open');
+  if (sidenav.classList.contains('open')) {
+    sidenav.classList.remove('open');
+    document.removeEventListener('click', hideSidenav)
+  } else {
+    sidenav.classList.add('open');
+    setTimeout(() => document.addEventListener('click', hideSidenav), 600) // transition duration
+  }
 }
 
 function hideSidenav(event) {
-  if(!event.target.closest('.sidenav') && !event.target.closest('.header__icon')) {
+  if(!sidenav.contains(event.target) && sidenav.classList.contains('open')) {
+    document.removeEventListener('click', hideSidenav)
     sidenav.classList.remove('open');
   }
 }
