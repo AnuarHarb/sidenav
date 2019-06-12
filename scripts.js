090b6e2be3778fef9181307fe00621f80c366df7
Marco Vinicio <mvglizarraga@gmail.com>
2019-06-12 18:11:12 -0500

    very nice stop propagation

--- scripts.js
+++ scripts.js
@@ -1,27 +1,28 @@
 'use strict';
 
-const sidenavTriggers = document.querySelectorAll('.sidenav-trigger');
-const sidenav = document.getElementById('sidenav');
+(function() {
+  const sidenavTriggers = document.querySelectorAll('.sidenav-trigger');
+  const sidenav = document.getElementById('sidenav');
 
-// document.addEventListener('click', hideSidenav);
+  // document.addEventListener('click', hideSidenav);
 
-for(let trigger of sidenavTriggers) {
-  trigger.addEventListener('click', toggleSidenav);
-}
-
-function toggleSidenav() {
-  if (sidenav.classList.contains('open')) {
-    sidenav.classList.remove('open');
-    document.removeEventListener('click', hideSidenav)
-  } else {
-    sidenav.classList.add('open');
-    setTimeout(() => document.addEventListener('click', hideSidenav), 600) // transition duration
+  for(let trigger of sidenavTriggers) {
+    trigger.addEventListener('click', (e) => {
+      e.stopPropagation()
+      if (sidenav.classList.contains('open')) {
+        sidenav.classList.remove('open');
+        document.removeEventListener('click', hideSidenav)
+      } else {
+        sidenav.classList.add('open');
+        document.addEventListener('click', hideSidenav)
+      }
+    });
   }
-}
 
-function hideSidenav(event) {
-  if(!sidenav.contains(event.target) && sidenav.classList.contains('open')) {
-    document.removeEventListener('click', hideSidenav)
-    sidenav.classList.remove('open');
+  function hideSidenav(event) {
+    if(!sidenav.contains(event.target) && sidenav.classList.contains('open')) {
+      document.removeEventListener('click', hideSidenav)
+      sidenav.classList.remove('open');
+    }
   }
-}
+})()
